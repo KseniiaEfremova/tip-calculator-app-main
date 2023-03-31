cd62d2b1f6bbe9bf17fae9bff4e205444fd32d09
KseniiaEfremova <ef.xenya@yandex.ru>
2023-03-31 10:48:00 +0100

    Added js for tip-bottons

--- assets/script.js
+++ assets/script.js
@@ -1,46 +1,126 @@
-let billSumEl = document.getElementById('bill-sum');
-let btns = document.querySelectorAll(".btn-tip");
+let button5 = document.getElementById("btn-5");
+let button10 = document.getElementById("btn-10");
+let button15 = document.getElementById("btn-15");
+let button25 = document.getElementById("btn-25");
+let button50 = document.getElementById("btn-50");
 
-let inputVal;
-let inputValperson;
-
-let numberPeople = document.getElementById("people-number");
-
-billSumEl.addEventListener("input", function (e) {
-    inputVal = parseInt(e.target.value);
-})
+let inputBill = document.getElementById("bill-sum");
 
 
-function calculateTipAmount(percentage) {
-    return (inputVal * percentage) / 100
-};
 
 
-btns.forEach(function (btn) {
-    // Pass in event as an argument
-    btn.addEventListener("click", function (event) {
-        // Log the event.currentTarget
+function getPercentage(tipValue) {
+    let billValue = Number(inputBill.value);
+    if (tipValue === "5%") {
+        res = billValue * 0.05
+        console.log(res);
 
-        percentage = parseInt(event.currentTarget.textContent);
+    } else if (tipValue === "10%") {
+        res = billValue * 0.10
+        console.log(res);
 
-        inputVal = calculateTipAmount(percentage);
-        console.log(inputVal);
-    });
-});
+    } else if (tipValue === "15%") {
+        res = billValue * 0.15
+        console.log(res);
 
+    } else if (tipValue === "25%") {
+        res = billValue * 0.25
+        console.log(res);
 
-numberPeople.addEventListener("input", function (e) {
-    inputValperson = inputVal / parseInt(e.target.value);
-    console.log(inputValperson);
-})
+    } else if (tipValue === "50%") {
+        res = billValue * 0.5;
+        console.log(res);
+    } else {
 
-console.log(inputValperson);
-
-
-function handleInput() {
-
+    }
 }
 
-function displayTotal(value) {
 
+function onGetPercentageButtonClick(eventObject) {
+    let clickedElement = eventObject.currentTarget;
+    let percent = clickedElement.textContent;
+    getPercentage(percent);
 }
+
+
+
+button5.addEventListener("click", onGetPercentageButtonClick);
+button10.addEventListener("click", onGetPercentageButtonClick);
+button15.addEventListener("click", onGetPercentageButtonClick);
+button25.addEventListener("click", onGetPercentageButtonClick);
+button50.addEventListener("click", onGetPercentageButtonClick);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let billSumEl = document.getElementById('bill-sum');
+// let btns = document.querySelectorAll(".btn-tip");
+
+// let inputVal;
+// let inputValperson;
+
+// let numberPeople = document.getElementById("people-number");
+// let tipAmount = document.getElementById("output-for-person");
+// let totalSum = document.getElementById("output-for-people");
+
+
+// // getting value fron input field
+// billSumEl.addEventListener("input", function (e) {
+//     inputVal = parseInt(e.target.value);
+// })
+
+
+// // creating a function to calculate percentage
+// function calculateTipAmount(percentage) {
+    
+//     return (inputVal * percentage) / 100
+// };
+
+
+// // calculating percentage after clicking a button
+
+// btns.forEach(function (btn) {
+
+//     btn.addEventListener("click", function (event) {
+//         percentage = parseInt(event.currentTarget.textContent);
+
+//         inputVal = calculateTipAmount(percentage);
+//         console.log(inputVal);
+//     });
+
+// });
+
+
+// // dividing the sum for one person
+// numberPeople.addEventListener("input", function (e) {
+//     inputValperson = inputVal / parseInt(e.target.value);
+
+//     displayTotal(inputValperson);
+//     displayTotalPerPerson(inputVal);
+   
+// })
+
+// function displayTotal(value) {
+//     console.log('hello')
+//    totalSum.textContent = value;
+// }
+
+
+// function displayTotalPerPerson(value) {
+//     tipAmount.textContent = value;
+// }
+
+
