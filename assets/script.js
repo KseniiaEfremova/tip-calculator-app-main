9137202aaa60d52746fe00bbf513eb8bfa27389a
KseniiaEfremova <ef.xenya@yandex.ru>
2023-03-31 11:27:15 +0100

    REfactored code and added input custom tip

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,39 @@
-let button5 = document.getElementById("btn-5");
-let button10 = document.getElementById("btn-10");
-let button15 = document.getElementById("btn-15");
-let button25 = document.getElementById("btn-25");
-let button50 = document.getElementById("btn-50");
-
+// getting input value
 let inputBill = document.getElementById("bill-sum");
 
+// getting input custom tip value
+let inputCustomTip = document.getElementById("custom-tip");
 
 
+// getting all tip-buttons in the list
+let listOfButtons = document.querySelectorAll(".tip-button")
 
+
+// the function to calculate percentage of bill
 function getPercentage(tipValue) {
     let billValue = Number(inputBill.value);
     if (tipValue === "5%") {
         res = billValue * 0.05
-        console.log(res);
 
     } else if (tipValue === "10%") {
         res = billValue * 0.10
-        console.log(res);
 
     } else if (tipValue === "15%") {
         res = billValue * 0.15
-        console.log(res);
 
     } else if (tipValue === "25%") {
         res = billValue * 0.25
-        console.log(res);
 
     } else if (tipValue === "50%") {
         res = billValue * 0.5;
-        console.log(res);
     } else {
 
     }
+    console.log(res);
+    return res;
 }
 
-
+// the function takes value of the clicked button
 function onGetPercentageButtonClick(eventObject) {
     let clickedElement = eventObject.currentTarget;
     let percent = clickedElement.textContent;
@@ -43,13 +41,11 @@ function onGetPercentageButtonClick(eventObject) {
 }
 
 
-
-button5.addEventListener("click", onGetPercentageButtonClick);
-button10.addEventListener("click", onGetPercentageButtonClick);
-button15.addEventListener("click", onGetPercentageButtonClick);
-button25.addEventListener("click", onGetPercentageButtonClick);
-button50.addEventListener("click", onGetPercentageButtonClick);
-
+//  the loop for choosing the clicked button and add listener and calculate percentage
+for (let i=0; i < listOfButtons.length; i++) {
+    let button = listOfButtons[i];
+    button.addEventListener("click", onGetPercentageButtonClick);
+}
 
 
 
@@ -57,8 +53,11 @@ button50.addEventListener("click", onGetPercentageButtonClick);
 
 
 
-
-
+// button5.addEventListener("click", onGetPercentageButtonClick);
+// button10.addEventListener("click", onGetPercentageButtonClick);
+// button15.addEventListener("click", onGetPercentageButtonClick);
+// button25.addEventListener("click", onGetPercentageButtonClick);
+// button50.addEventListener("click", onGetPercentageButtonClick);
 
 
 
